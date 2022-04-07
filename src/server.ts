f39165fabeb70df2a25399fe09f04a47ccb93dc4
FernandodotDev <fernandodossantos005@gmail.com>
2022-04-07 19:48:08 -0300

    Port heroku

--- src/server.ts
+++ src/server.ts
@@ -43,6 +43,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 
 dotenv.config();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server started on port 3000');
 });
