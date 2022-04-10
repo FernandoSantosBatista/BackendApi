6b095b37e4797e53eb58ee3bd4b2e5cec0ca8f09
FernandodotDev <fernandodossantos005@gmail.com>
2022-04-10 11:11:16 -0300

    o-o

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,15 @@
-import { createConnection } from 'typeorm';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
+import { createConnection } from 'typeorm';
 
-const rootDir = process.env.NODE_ENV === "development" ? "src" : "dist";
+const rootDir = process.env.NODE_ENV === 'development' ? './src' : './dist';
 
-const extensionFile = process.env.NODE_ENV === "development" ? "ts" : "js";
+const extensionFile = process.env.NODE_ENV === 'development' ? 'ts' : 'js';
 
 const config: any = {
   type: 'postgres',
   host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT || 5432,
+  port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
@@ -18,19 +18,15 @@ const config: any = {
     ssl: {
       require: true,
       rejectUnauthorized: false,
-    }
+    },
   },
-  entities: [
-    rootDir + `/models/*.${extensionFile}`
-  ],
+  entities: [rootDir + `/models/*.${extensionFile}`],
 
-  migrations: [
-    rootDir + `/database/migrations/*.${extensionFile}`
-  ],
+  migrations: [rootDir + `/database/migrations/*.${extensionFile}`],
 
   cli: {
     migrationsDir: rootDir + '/database/migrations',
-  }
+  },
 };
 
-createConnection(config);
+createConnection(config).catch(error => console.log(error));
